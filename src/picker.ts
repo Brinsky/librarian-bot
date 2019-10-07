30fe81e4db92f35f983f4ffcd819bb74194daf45
Brian Stottler <brianstottler@gmail.com>
2019-10-06 22:27:45 -0400

    Change !picker to accept arguments of any kind

--- src/picker.ts
+++ src/picker.ts
@@ -1,31 +1,9 @@
-import {Client, Message, User} from 'discord.js'
+import {Message} from 'discord.js'
 import {FlagsAndArgs} from './command'
-import {ClientError} from './error'
 import {randomInt} from './util'
 
-const TAG_PATTERN = /<@(\d+)>/;
-
-export async function picker(flagsAndArgs: FlagsAndArgs, message: Message,
-  client: Client): Promise<void> {
-  const users: User[] = [];
-
-  for(const tag of flagsAndArgs.args) {
-    // Validate @ tag and extract user ID
-    const match = tag.match(TAG_PATTERN);
-    if (match === null || match.length !== 2) { 
-      throw new ClientError(`Unrecognized argument ${tag}. Expect an @ tag`);
-    }
-    const id = match[1];
-
-    // Ensure each ID corresponds to a real user
-    try {
-      users.push(await client.users.fetch(id));
-    } catch(err) {
-      throw new ClientError(`Failed to find user with ID ${id}`, err);
-    }
-  }
-
-  const pickedIndex = randomInt(0, users.length); 
-  
-  message.channel.send(`I picker ${users[pickedIndex]}!`);
+export async function picker(
+  flagsAndArgs: FlagsAndArgs, message: Message): Promise<void> {
+  const pickedIndex = randomInt(0, flagsAndArgs.args.length); 
+  message.channel.send(`I picker ${flagsAndArgs.args[pickedIndex]}!`);
 }
